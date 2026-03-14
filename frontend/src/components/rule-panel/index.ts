86955240853601445cca620206751e36fea490a5
Merlijn Tishauser <merlijn@ccadd.nl>
2026-03-14 08:24:21 +0100

    Run vite directly in Dockerfile to silence npm SIGTERM noise on shutdown
    Also remove unused re-exports (RuleDetails, SimFormState, RuleCardProps)
    from rule-panel barrel to fix react-doctor score.

--- frontend/src/components/rule-panel/index.ts
+++ frontend/src/components/rule-panel/index.ts
@@ -1,9 +1,6 @@
 export { default as RuleCard } from "./RuleCard";
-export { default as RuleDetails } from "./RuleDetails";
 export { default as SimulationForm } from "./SimulationForm";
 export { default as SimulationResult } from "./SimulationResult";
 export { default as FindingsList } from "./FindingsList";
 export { default as AiAnalysisStatus } from "./AiAnalysisStatus";
 export { gradeColor, initialFormState, formReducer, buildSimulateRequest, deriveAiError, deriveMutationError } from "./utils";
-export type { SimFormState } from "./utils";
-export type { RuleCardProps } from "./RuleCard";
